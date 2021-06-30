a01dc7e7ba44afcf220cd6b69aaebc2b1c37c0d5
Harry Hallows <hhallows96@gmail.com>
2021-06-30 17:04:46 +0100

    minimap done

--- CloudCities/Assets/Scripts/System/Camera/FollowTarget.cs
+++ CloudCities/Assets/Scripts/System/Camera/FollowTarget.cs
@@ -8,8 +8,34 @@ public class FollowTarget : MonoBehaviour
 
     [SerializeField] public float smoothSpeed;
 
+    [SerializeField] private bool isMinimap;
+
     // Update is called once per frame
     void LateUpdate()
+    {
+
+        if (isMinimap)
+        {
+            Minimap();
+        }
+        else
+        {
+            Follow();
+        }
+       
+    }
+
+    private void Minimap()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.y = transform.position.y;
+
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, target.eulerAngles.y, 0f);
+    }
+
+    private void Follow()
     {
         Vector3 desiredPosition = target.position;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
